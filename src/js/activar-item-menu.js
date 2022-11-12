e016846d4b3aef7cfa25671a192e8ba53756276e
carolinaherrera19 <lifaherrera@gmail.com>
2022-11-12 10:56:50 -0500

    agregando navbar en las paginas por ciudad

--- src/js/activar-item-menu.js
+++ src/js/activar-item-menu.js
@@ -1,17 +1,12 @@
 function activarItemMenu() {
     const itemsMenu = [
-        { id: 'item-menu-bucaramanga', link: './bucaramanga' },
-        { id: 'item-menu-bogota', link: './bogota' },
-        { id: 'item-menu-medellin', link: './medellin' },
+        { id: 'item-menu-bogota', link: '/src/page/rutas_Bogota.html' },
+        { id: 'item-menu-medellin', link: '/src/page/rutas_Medellin.html' },
     ]
     itemsMenu.forEach(item => {
-        document.getElementById(item.id).addEventListener('click', () => {
-            const elementA = document.querySelectorAll('a.nav-link')
-            elementA.forEach(a => {
-                a.id === item.id ? a.classList.add('active') : a.classList.remove('active')
-            })
-            window.location.href = item.link
-        })
-    });
+        if (item.link === window.location.pathname) {
+            document.getElementById(item.id).classList.add('active')
+        }
+    })
 }
 activarItemMenu()
